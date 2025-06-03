caf283207eb7889b342b2b653441ce6bf898f6df
ggmarts04 <ggmart04@gmail.com>
2025-06-03 13:21:45 +0530

    Update inference.py

--- scripts/inference.py
+++ scripts/inference.py
@@ -79,7 +79,7 @@ def main(config, args):
 
     print(f"Initial seed: {torch.initial_seed()}")
 
-    mask_path_to_use = getattr(config.data, "mask_image_path", "latentsync/utils/mask3.png")
+    mask_path_to_use = getattr(config.data, "mask_image_path", "latentsync/utils/maskeer.png")
     pipeline(
         video_path=args.video_path,
         audio_path=args.audio_path,
